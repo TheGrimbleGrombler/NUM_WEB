6253255de9ec80bb5fc9e907b511ea23796a6f3c
Glitch (glitch-blank-website) <none>
2023-12-16 19:18:31 +0000

    👯👚 Checkpoint ./script.js:63677986/365

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const Decimal = window.Decimal;
 
+function E(n) {
+  
+  return new Decimal().fromString(n)
+  
+}
 var x = new Decimal().fromString("1e100")
 var StardustDisplay = document.getElementById("StardustDisplay");
 
 function updateText() {
-  x.equals(x.mul());
+  x = x.mul(E("2"));
+  console.log(x)
   StardustDisplay.innerHTML = String(x);
 }
-var s = E(100)
 
 setInterval(updateText, 16);
 
