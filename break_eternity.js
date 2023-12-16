e2f46faf8fc10aa47f2ce7a4f8472c5097a6b4e2
Glitch (glitch-blank-website) <none>
2023-12-16 19:41:34 +0000

    💑🚴 Checkpoint ./style.css:63677986/206 ./break_eternity.js:63677986/10 ./script.js:63677986/486

--- break_eternity.js
+++ break_eternity.js
@@ -178,7 +178,7 @@
     this.value = value;
   });
 
-  var MAX_SIGNIFICANT_DIGITS = 17; //Maximum number of digits of precision to assume in Number
+  var MAX_SIGNIFICANT_DIGITS = 15; //Maximum number of digits of precision to assume in Number
   var EXP_LIMIT = 9e15; //If we're ABOVE this value, increase a layer. (9e15 is close to the largest integer that can fit in a Number.)
   var LAYER_DOWN = Math.log10(9e15);
   var FIRST_NEG_LAYER = 1 / 9e15; //At layer 0, smaller non-zero numbers than this become layer 1 numbers with negative mag. After that the pattern continues as normal.
--- script.js
+++ script.js
@@ -5,15 +5,41 @@ function E(n) {
   return new Decimal().fromString(n)
   
 }
+
+let player = {
+  stardust: E("0")
+  
+}
+
 var x = new Decimal().fromString("1e100")
 var StardustDisplay = document.getElementById("StardustDisplay");
 
 function updateText() {
-  x = x.mul(E("2"));
-  console.log(x)
-  StardustDisplay.innerHTML = String(x);
+
+  gainstardust()
+  
+  
+  StardustDisplay.innerHTML = String(player.stardust);
 }
 
 setInterval(updateText, 16);
+
+function gainstardust(){
+  
+  var gain = E("1")
+  
+  
+  var gain = gain.div(60)
+  player.stardust = player.stardust.add(gain)
+}
+
+
+
+
+
+
+
+
+
 
 updateText();
