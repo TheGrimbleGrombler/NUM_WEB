941930290368b0b7e3827b843c869b8d9c91da44
Glitch (glitch-blank-website) <none>
2023-12-16 21:22:42 +0000

    🏪🐗 Checkpoint ./index.html:63677986/156 ./style.css:63677986/512 ./script.js:63677986/1048

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function E(n) {
 }
 let buyables = {
   syphon: {cost: E("1"), amount: E("0")}
+  syphon2: {cost: E("100"), amount: E("0")}
 
 }
 let player = {
@@ -17,6 +18,9 @@ let player = {
 var x = new Decimal().fromString("1e100")
 var StardustDisplay = document.getElementById("StardustDisplay");
 var SyphonDisplay = document.getElementById("SyphonDisplay");
+var SyphonButton = document.getElementById("SyphonButton");
+var SyphonDisplay2 = document.getElementById("SyphonDisplay2");
+var SyphonButton2 = document.getElementById("SyphonButton2");
 
 function updateText() {
 
@@ -25,12 +29,16 @@ function updateText() {
   
   StardustDisplay.innerHTML = String(player.stardust);
   SyphonDisplay.innerHTML = String(buyables.syphon.amount);
+  SyphonButton.innerHTML = "Cost: " + String(buyables.syphon.cost) + " Stardust";
+  SyphonDisplay2.innerHTML = String(buyables.syphon2.amount);
+  SyphonButton2.innerHTML = "Cost: " + String(buyables.syphon2.cost) + " Stardust";
 }
 
 setInterval(updateText, 16);
 
 function gainstardust(){
   
+  buyables.syphon.amount = buyables.syphon.amount.add(buyables.syphon2.amount.div(E("60")))
   var gain = E("0")
   gain = gain.add(buyables.syphon.amount)
   
@@ -45,6 +53,12 @@ document.getElementById('SyphonButton').addEventListener('click', function() {
     player.stardust = player.stardust.sub(buyables.syphon.cost)
     buyables.syphon.amount = buyables.syphon.amount.add(E("1"))
     buyables.syphon.cost = buyables.syphon.cost.mul(E("2"))
+}});
+document.getElementById('SyphonButton2').addEventListener('click', function() {
+  if (player.stardust.gte(buyables.syphon2.cost)) {
+    player.stardust = player.stardust.sub(buyables.syphon2.cost)
+    buyables.syphon2.amount = buyables.syphon2.amount.add(E("1"))
+    buyables.syphon2.cost = buyables.syphon2.cost.mul(E("2"))
 }});
 
 
