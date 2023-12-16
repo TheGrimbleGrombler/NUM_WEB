ef84b525dd0872953455cd7cdf35c01aa723ef1a
Glitch (glitch-blank-website) <none>
2023-12-16 21:00:08 +0000

    🏥👢 Checkpoint ./index.html:63677986/818 ./script.js:63677986/1008

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ function E(n) {
   return new Decimal().fromString(n)
   
 }
+let buyables = {
+  syphon: {cost: 1, amount: 0}
 
+}
 let player = {
-  stardust: E("0")
-  
+  stardust: E("1")
+
 }
 
 var x = new Decimal().fromString("1e100")
@@ -26,7 +29,8 @@ setInterval(updateText, 16);
 
 function gainstardust(){
   
-  var gain = E("1")
+  var gain = E("0")
+  gain = gain.add(buyables.syphon.amount)
   
   
   var gain = gain.div(60)
@@ -34,7 +38,12 @@ function gainstardust(){
 }
 
 
-
+document.getElementById('SyphonButton').addEventListener('click', function() {
+  if (player.stardust.gte(buyables.syphon.cost)) {
+    player.stardust = player.stardust.sub(buyables.syphon.cost)
+    buyables.syphon.amount = buyables.syphon.amount.add(E("1"))
+    buyables.syphon.cost = buyables.syphon.cost.mul(E("2"))
+}});
 
 
 
