17956cbaf23d7ba91f82854c4f8b4fdb89cec08f
Glitch (glitch-blank-website) <none>
2023-12-16 02:17:13 +0000

    💅🍭 Checkpoint ./index.html:63677986/1409 ./script.js:63677986/2848 ./break_eternity.js:63677986/558990 ./LICENSE.md:63677986/1072

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-import Decimal from './<link>Break_Eternity.js</link>'
+import Decimal from "Break_Eternity.js";
 
-var x = new Decimal("1e100")
+var x = new Decimal("1e100");
 var StardustDisplay = document.getElementById("StardustDisplay");
 
-function doSomethingEveryFrame() {
-  
-  x.equals(x.mult(100))
-  StardustDisplay.value = String(x);
-  
-  requestAnimationFrame(doSomethingEveryFrame);
+function updateText() {
+  x.equals(x.mult(100));
+  StardustDisplay.innerHTML = String(x);
 }
-doSomethingEveryFrame();
+
+setInterval(updateText, 16);
+
+updateText();
