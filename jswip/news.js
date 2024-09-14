098d6fd049cb4b7630eed5a099911eec4cf2ce47
Glitch (glitch-blank-website) <none>
2024-09-14 15:37:12 +0000

    🐜🍖 Checkpoint ./jswip/news.js:63677986/679 ./index.html:63677986/2886 ./style.css:63677986/241

--- jswip/news.js
+++ jswip/news.js
@@ -1,7 +1,28 @@
 var tick = 0;
 
+const newstexts = [
+  "HEY HEY HEY!!!!",
+  "What the rizz?",
+  "Back in my day we didn't have these darn videogames.",
+  "Breaking news: Florginshammerinkledroohashlir."
+]
+
+var current = 3
+
+var textbox = document.getElementById("NewsText")
+
 function news() {
   tick += 1;
+  if (tick == 1) {
+    current = 3
+  }
+  
+  textbox.style.left = String(100 - tick) + "%"
+  textbox.innerHTML = newstexts[current]
+  
+  if (tick >= 200) {
+    tick = 0
+  }
   
 }
 
