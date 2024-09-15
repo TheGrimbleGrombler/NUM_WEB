7d4aadcaae0eb555c67d2e432f7b6abc3457e4a7
Glitch (glitch-blank-website) <none>
2024-09-15 01:18:11 +0000

    🐊🏗 Checkpoint ./jswip/guihandler.js:63677986/1275 ./index.html:63677986/102

--- jswip/guihandler.js
+++ jswip/guihandler.js
@@ -1,6 +1,8 @@
 
 var chosen = null
+var chosenbutton = null
 var chosenS = null
+var chosenSbutton = null
 
 window.TABS = {
   choose: function(tabN,subtab) {
@@ -9,25 +11,42 @@ window.TABS = {
       if (chosenS != null) {
         chosenS.style.display = "none"
         chosenS = null
+        if (chosenSbutton != null) {
+          chosenSbutton.class = "btn_tab"
+          chosenSbutton = null
+        }
       }
       chosenS = document.getElementById("subtab" + tabN)
+      chosenSbutton = document.getElementById("subtab" + tabN + "button")
     } else {
       if (chosen != null) {
         chosen.style.display = "none"
         chosen = null
+        if (chosenbutton != null) {
+          chosenbutton.class = "btn_tab"
+          chosenbutton = null
+        }
       }
       chosen = document.getElementById("tab" + tabN)
+      chosenbutton = document.getElementById("tab" + tabN + "button")
     }
     
     if (chosen != null) {
       chosen.style.display = ""
     }
+    if (chosenbutton != null) {
+      chosenbutton.class = "btn_tab chosen"
+    }
     if (chosenS != null) {
       chosenS.style.display = ""
+    }
+    if (chosenSbutton != null) {
+      chosenSbutton.class = "btn_tab chosen"
     }
   }
 }
 document.addEventListener("DOMContentLoaded", function() {
   window.TABS.choose("0", false)
   window.TABS.choose("0", true)
+  document.getElementById("tab0button").class = "btn_tab chosen"
 });
