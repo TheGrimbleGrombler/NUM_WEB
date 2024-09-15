75590fd8a4b54051285ee356819993e521a6d10b
Glitch (glitch-blank-website) <none>
2024-09-15 13:47:41 +0000

    🌁🎻 Checkpoint ./.glitch-assets:63677986/1747 ./jswip/upgradesV2.js:63677986/1761 ./index.html:63677986/1615

--- jswip/upgradesV2.js
+++ jswip/upgradesV2.js
@@ -7,11 +7,30 @@ function E(n) {
 }
 
 window.UPGRADES = {
-  9: {
+  0: {
     efficiencyI: {
       displayName: "Efficency - I",
       description: "Double Data gain",
-      image: "",
+      image: "https://cdn.glitch.global/5ecf2822-9c4a-4be8-9d23-05f1ff302473/Eff1.png?v=1726407804499",
+      costType: "Data",
+      costAmount: function() {
+        var temp = E("1000")
+        
+        return temp;
+      },
+      effectPrefix: "Currently: Data x",
+      effectSuffix: "!",
+      bought: false,
+      effect: function() {
+        var temp = E("1")
+        if (this.bought == true) {temp = E("2")}
+        return temp;
+      },
+    }
+    efficiencyII: {
+      displayName: "Efficency - II",
+      description: "Double Data gain",
+      image: "https://cdn.glitch.global/5ecf2822-9c4a-4be8-9d23-05f1ff302473/Eff1.png?v=1726407804499",
       costType: "Data",
       costAmount: function() {
         var temp = E("1000")
